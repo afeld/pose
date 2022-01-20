b2d54fe5cfcdaef903f3b0665bdadb35906ec372
Aidan Feldman <aidan.feldman@gmail.com>
2022-01-20 13:28:32 -0500

    display webcam

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,20 +1,19 @@
 import "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
 
-const img = document.getElementById("image");
+const img = document.getElementById("image") as HTMLImageElement;
+const video = document.getElementById("video") as HTMLVideoElement;
 
 async function loadAndPredict() {
   const net = await bodyPix.load();
 
-  /**
-   * One of (see documentation below):
-   *   - net.segmentPerson
-   *   - net.segmentPersonParts
-   *   - net.segmentMultiPerson
-   *   - net.segmentMultiPersonParts
-   * See documentation below for details on each method.
-   */
   const segmentation = await net.segmentPerson(img);
   console.log(segmentation);
 }
 loadAndPredict();
+
+const setUpWebcam = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+};
+setUpWebcam();
