40fd74fc10fd6617a2883ca35e62d73c1729eb41
Aidan Feldman <aidan.feldman@gmail.com>
2022-01-23 00:57:09 -0500

    refactor mask and skeleton drawing functions to their own file

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,16 +1,11 @@
 import "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
 import Stats from "stats.js";
-import Skeleton from "./skeleton";
 import Video from "./video";
-import { Color } from "@tensorflow-models/body-pix/dist/types";
 import Canvas from "./canvas";
 import Detector from "./detector";
 import { getElementById } from "./dom_helpers";
-
-const COLOR_CLEAR = { r: 0, g: 0, b: 0, a: 0 } as Color;
-const COLOR_RED = { r: 255, g: 0, b: 0, a: 255 } as Color;
-const COLOR_GREEN = { r: 0, g: 255, b: 0, a: 255 } as Color;
+import { drawMask, drawSkeleton } from "./segment_helpers";
 
 // effects
 let freezeFrame: bodyPix.SemanticPersonSegmentation | undefined;
@@ -19,53 +14,6 @@ const cannon = [] as bodyPix.SemanticPersonSegmentation[];
 const showFPS = (stats: Stats) => {
   stats.showPanel(0);
   document.body.appendChild(stats.dom);
-};
-
-const getMask = (
-  segmentation: bodyPix.SemanticPersonSegmentation,
-  color = COLOR_RED
-) => {
-  const backgroundColor = COLOR_CLEAR;
-  const drawContour = true;
-
-  return bodyPix.toMask(segmentation, color, backgroundColor, drawContour);
-};
-
-const drawMask = (
-  segmentation: bodyPix.SemanticPersonSegmentation,
-  canvas: HTMLCanvasElement,
-  color = COLOR_RED
-) => {
-  // TODO move up to be a constant. Moved here because the size wasn't correct when at the top of the file.
-  const EMPTY_BACKGROUND = new Image(canvas.clientWidth, canvas.clientHeight);
-
-  const coloredPartImage = getMask(segmentation, color);
-  const opacity = 1;
-  const flipHorizontal = true;
-  const maskBlurAmount = 0;
-
-  bodyPix.drawMask(
-    canvas,
-    EMPTY_BACKGROUND,
-    coloredPartImage,
-    opacity,
-    maskBlurAmount,
-    flipHorizontal
-  );
-};
-
-const drawSkeleton = (
-  segmentation: bodyPix.SemanticPersonSegmentation,
-  canvas: Canvas
-) => {
-  let pose = segmentation.allPoses[0];
-  if (!pose) {
-    // no people found
-    return;
-  }
-  pose = bodyPix.flipPoseHorizontal(pose, segmentation.width);
-  const skeleton = new Skeleton(pose);
-  skeleton.draw(canvas);
 };
 
 const loadAndPredict = async (
