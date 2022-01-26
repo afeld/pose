a423560e462b53155101da7bd3e07dc66413003f
Aidan Feldman <aidan.feldman@gmail.com>
2022-01-26 01:00:56 -0500

    refactor listener callbacks to be methods

--- src/js/listener_controller.ts
+++ src/js/listener_controller.ts
@@ -3,36 +3,39 @@ import { getElementById } from "./dom_helpers";
 import Effect from "./effects/effect";
 import Listener from "./listener";
 
-const onVoiceCommand = (effects: Effect[], command: string) => {
-  console.log("command:", command);
-
-  const action = actions.find((action) => action.commands.includes(command));
-  if (!action) {
-    console.warn(`command "${command}" not found`);
-    return;
-  }
-  action.callback(effects);
-};
-
 export default class ListenerController {
   el: HTMLInputElement;
   listener: Listener;
+  effects: Effect[];
 
   constructor(effects: Effect[]) {
     const allCommands = actions.map((action) => action.commands).flat();
     this.listener = new Listener(allCommands);
-    this.listener.onCommand((command) => onVoiceCommand(effects, command));
+    this.effects = effects;
+    this.listener.onCommand(this.onVoiceCommand);
 
     this.el = getElementById("enable-speech") as HTMLInputElement;
-
-    this.el.addEventListener("change", () => {
-      if (this.el.checked) {
-        this.startIfAllowed();
-      } else {
-        this.stop();
-      }
-    });
+    this.el.addEventListener("change", this.onCheckboxChange);
   }
+
+  onVoiceCommand = (command: string) => {
+    console.log("command:", command);
+
+    const action = actions.find((action) => action.commands.includes(command));
+    if (!action) {
+      console.warn(`command "${command}" not found`);
+      return;
+    }
+    action.callback(this.effects);
+  };
+
+  onCheckboxChange = () => {
+    if (this.el.checked) {
+      this.startIfAllowed();
+    } else {
+      this.stop();
+    }
+  };
 
   isAllowed() {
     return this.el.checked;
