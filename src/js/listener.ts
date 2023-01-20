b2f71a10c7be4334f672b7d6c669bfe154a85631
Aidan Feldman <aidan.feldman@gmail.com>
2023-01-20 13:33:53 -0500

    once bluetooth connects, restart listener

--- src/js/listener.ts
+++ src/js/listener.ts
@@ -4,7 +4,11 @@
 import * as speechCommands from "@tensorflow-models/speech-commands";
 import { EventEmitter } from "events";
 
-// returns the index of the maximum value in an array
+const DEFAULT_DEVICE_ID = "default";
+
+/**
+ * @returns the index of the maximum value in an array
+ */
 const indexOfMax = (arr: Float32Array) => {
   // https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array#comment54083228_30850912
   return arr.reduce(
@@ -14,6 +18,24 @@ const indexOfMax = (arr: Float32Array) => {
         : bestIndexSoFar,
     0
   );
+};
+
+/**
+ * @returns the device ID of the first bluetooth audio input device, if available
+ */
+const getDeviceID = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  let device = devices.find(
+    (device) =>
+      device.kind === "audioinput" &&
+      device.deviceId !== DEFAULT_DEVICE_ID &&
+      device.label.toLowerCase().includes("bluetooth")
+  );
+
+  if (device) {
+    return device.deviceId;
+  }
+  return DEFAULT_DEVICE_ID;
 };
 
 export default class Listener {
@@ -47,15 +69,50 @@ export default class Listener {
     this.eventEmitter.on("command", callback);
   }
 
-  start() {
-    this.recognizer.listen(this.listenCallback, {
+  async start(deviceId: string | undefined = undefined) {
+    if (this.recognizer.isListening()) {
+      return;
+    }
+
+    if (!deviceId) {
+      deviceId = await getDeviceID();
+    }
+    console.log("Using audio input device", deviceId);
+
+    if (deviceId === DEFAULT_DEVICE_ID) {
+      this.waitForBluetooth();
+    }
+
+    await this.recognizer.listen(this.listenCallback, {
       probabilityThreshold: 0.99,
       // the recognizer seems to pick up duplicate commands sometimes, so only take the first one
       suppressionTimeMillis: 1500,
+      audioTrackConstraints: {
+        deviceId: deviceId,
+      },
     });
   }
 
-  stop() {
-    this.recognizer.stopListening();
+  /**
+   * it takes a moment for bluetooth devices to get connected, so wait to see if one does
+   */
+  waitForBluetooth() {
+    const intervalID = setInterval(async () => {
+      const deviceId = await getDeviceID();
+      if (deviceId === DEFAULT_DEVICE_ID) {
+        return;
+      }
+
+      clearInterval(intervalID);
+      console.log("Reconnecting audio");
+      await this.stop();
+      this.start(deviceId);
+    }, 1000);
+  }
+
+  async stop() {
+    if (this.recognizer.isListening()) {
+      await this.recognizer.stopListening();
+    }
   }
 }
--- src/js/sandbox.ts
+++ src/js/sandbox.ts
@@ -1,58 +1,8 @@
-// https://github.com/tensorflow/tfjs-models/tree/master/speech-commands#online-streaming-recognition
+const start = Date.now();
 
-import * as speechCommands from "@tensorflow-models/speech-commands";
-
-// returns the index of the maximum value in an array
-const indexOfMax = (arr: Float32Array) => {
-  // https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array#comment54083228_30850912
-  return arr.reduce(
-    (bestIndexSoFar, currentlyTestedValue, currentlyTestedIndex, array) =>
-      currentlyTestedValue > array[bestIndexSoFar]
-        ? currentlyTestedIndex
-        : bestIndexSoFar,
-    0
-  );
-};
-
-const run = async () => {
-  // When calling `create()`, you must provide the type of the audio input.
-  // The two available options are `BROWSER_FFT` and `SOFT_FFT`.
-  // - BROWSER_FFT uses the browser's native Fourier transform.
-  // - SOFT_FFT uses JavaScript implementations of Fourier transform
-  //   (not implemented yet).
-  const recognizer = speechCommands.create("BROWSER_FFT");
-
-  // Make sure that the underlying model and metadata are loaded via HTTPS
-  // requests.
-  await recognizer.ensureModelLoaded();
-
-  // See the array of words that the recognizer is trained to recognize.
-  console.log(recognizer.wordLabels());
-
-  const onCommand = async (
-    result: speechCommands.SpeechCommandRecognizerResult
-  ) => {
-    // - result.scores contains the probability scores that correspond to
-    //   recognizer.wordLabels().
-    // - result.spectrogram contains the spectrogram of the recognized word.
-
-    const maxIndex = indexOfMax(result.scores as Float32Array);
-    const score = result.scores[maxIndex];
-    const command = recognizer.wordLabels()[maxIndex];
-    console.log(`command: ${command}, score: ${score}`);
-  };
-
-  // `listen()` takes two arguments:
-  // 1. A callback function that is invoked anytime a word is recognized.
-  // 2. A configuration object with adjustable fields such a
-  //    - includeSpectrogram
-  //    - probabilityThreshold
-  //    - includeEmbedding
-  recognizer.listen(onCommand, {
-    probabilityThreshold: 0.99,
-    // the recognizer seems to pick up duplicate commands sometimes, so only take the first one
-    suppressionTimeMillis: 1500,
-  });
-};
-
-run();
+setInterval(async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const inputDevices = devices.filter((device) => device.kind === "audioinput");
+  const labels = inputDevices.map((device) => device.label);
+  console.log(labels);
+}, 100);
