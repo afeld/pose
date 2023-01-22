34c19480921909cba797d86874d09c44133912c3
Aidan Feldman <aidan.feldman@gmail.com>
2023-01-22 16:15:38 -0500

    declare types for mic and vosk

--- src/js/npm.d.ts
+++ src/js/npm.d.ts
@@ -1,2 +1,34 @@
-declare module "mic";
-declare module "vosk";
+declare module "mic" {
+  interface MicOpts {
+    rate: string;
+    channels: string;
+    debug: boolean;
+    device: string;
+  }
+
+  function mic(options: MicOpts): any;
+  export = mic;
+}
+
+declare module "vosk" {
+  function setLogLevel(level: number): void;
+
+  class Model {
+    constructor(modelPath: string);
+    free(): void;
+  }
+
+  interface RecognizerOpts {
+    model: Model;
+    sampleRate: number;
+    grammar: string[];
+  }
+
+  class Recognizer {
+    constructor(opts: RecognizerOpts);
+    acceptWaveform(data: any): boolean;
+    result(): string;
+    finalResult(): string;
+    free(): void;
+  }
+}
