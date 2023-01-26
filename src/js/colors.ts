2b3690d471794cccd3c47a1bc06e57c24c29d589
Aidan Feldman <aidan.feldman@gmail.com>
2023-01-26 03:10:47 -0500

    generate colors automatically

--- src/js/colors.ts
+++ src/js/colors.ts
@@ -1,16 +1,12 @@
 import { Color } from "@tensorflow-models/pose-detection/dist/shared/calculators/interfaces/common_interfaces";
+import ColorScale from "color-scales";
 
 export const BLACK: Color = { r: 0, g: 0, b: 0, a: 255 };
 export const CLEAR: Color = { r: 0, g: 0, b: 0, a: 0 };
 
-// https://coolors.co/f9c80e-f86624-ea3546-662e9b-43bccd
-const COLORS: Color[] = [
-  { r: 249, g: 200, b: 14, a: 255 }, // yellow
-  { r: 248, g: 102, b: 36, a: 255 }, // orange
-  { r: 234, g: 53, b: 70, a: 255 }, // pink
-  { r: 102, g: 46, b: 155, a: 255 }, // purple
-  { r: 67, g: 188, b: 205, a: 255 }, // blue
-];
+// https://coolors.co/f86624-f14e35-ea3546-a83271-873086-662e9b
+const NUM = 6;
+const scale = new ColorScale(0, NUM - 1, ["#49008E", "#EA3546", "#FCC52F"]);
 
 let counter = 0;
 
@@ -18,9 +14,14 @@ let counter = 0;
  * cycles through the colors
  */
 export const getNext = () => {
-  const colorIndex = counter % (COLORS.length - 1);
+  const colorIndex = counter % (NUM - 1);
+
+  const color = scale.getColor(colorIndex);
+  // their alpha scale goes 0-1
+  color.a = 255;
+
   counter += 1;
-  return COLORS[colorIndex];
+  return color;
 };
 
 export const toString = (color: Color) =>
