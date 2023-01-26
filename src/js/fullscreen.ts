db4792f3e8baf721ce995a87f861bef5e74d7e03
Aidan Feldman <aidan.feldman@gmail.com>
2023-01-26 04:11:26 -0500

    fix permissions error when going fullscreen

--- src/js/fullscreen.ts
+++ src/js/fullscreen.ts
@@ -1,35 +1,19 @@
-import { fullscreenController } from "./controls";
+import { config, fullscreenController } from "./controls";
 import Canvas from "./canvas";
 
-// https://fettblog.eu/typescript-typing-catch-clauses/
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const onFullscreenError = (error: any) => {
-  if (
-    error instanceof TypeError &&
-    error.message === "Permissions check failed"
-  ) {
-    // there seems to be a bug in Chrome where it wants you to check for fullscreen permissions, but doing so isn't supported since it isn't part of this list (as of 1/26/23):
-    // https://chromium.googlesource.com/chromium/src/+/refs/heads/main/third_party/blink/renderer/modules/permissions/permission_descriptor.idl
-    // therefore, we'll just ignore it
-  } else {
-    throw error;
-  }
-};
-
-const onFullscreenChange = async (wantsFullscreen: boolean, canvas: Canvas) => {
-  if (wantsFullscreen) {
-    try {
-      await canvas.el.requestFullscreen();
-    } catch (error) {
-      onFullscreenError(error);
-    }
+const onFullscreenChange = async (canvas: Canvas) => {
+  if (config.fullscreen) {
+    await canvas.el.requestFullscreen();
   }
 };
 
 export const setupFullscreen = (canvas: Canvas) => {
-  fullscreenController.onChange((wantsFullscreen) =>
-    onFullscreenChange(wantsFullscreen, canvas)
+  // need to subscribe to the element instead of the Controller due to
+  // https://javascript.plainenglish.io/user-gesture-restricted-web-apis-d794454453f7
+  fullscreenController.domElement.addEventListener("change", () =>
+    onFullscreenChange(canvas)
   );
+
   document.addEventListener("fullscreenchange", () =>
     fullscreenController.setValue(!!document.fullscreenElement)
   );
