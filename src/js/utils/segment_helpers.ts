48f67256137499e314d8071a883703c9f2aebc49
Aidan Feldman <aidan.feldman@gmail.com>
2023-01-28 02:59:32 -0500

    make the mask opaque

--- src/js/utils/segment_helpers.ts
+++ src/js/utils/segment_helpers.ts
@@ -2,23 +2,31 @@ import Skeleton from "../poses/skeleton";
 import { Pose } from "@tensorflow-models/pose-detection";
 import { Segmentation } from "@tensorflow-models/pose-detection/dist/shared/calculators/interfaces/common_interfaces";
 import Canvas from "../display/canvas";
-import { BLACK, toString } from "./colors";
+import { BLACK } from "./colors";
 import { Color } from "@tensorflow-models/pose-detection/dist/shared/calculators/interfaces/common_interfaces";
 
-const replaceColor = (ctx: OffscreenCanvasRenderingContext2D, color: Color) => {
-  const colorStr = toString(color);
-  // https://stackoverflow.com/a/16228281/358804
-  ctx.fillStyle = colorStr;
-  ctx.globalCompositeOperation = "source-in";
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+/**
+ * @param imageData gets modified
+ */
+const replaceColor = (imageData: ImageData, color: Color) => {
+  // https://stackoverflow.com/a/16228779/358804
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const alpha = imageData.data[i + 3];
+    if (alpha > 0) {
+      // modify the color
+      imageData.data[i] = color.r;
+      imageData.data[i + 1] = color.g;
+      imageData.data[i + 2] = color.b;
+      imageData.data[i + 3] = color.a;
+    }
+  }
 };
 
 let canvas2: OffscreenCanvas | undefined;
 
 const drawImageDataWithTransparency = (
   canvas: Canvas,
-  imageData: ImageData,
-  color = BLACK
+  imageData: ImageData
 ) => {
   const ctx = canvas.context();
 
@@ -38,8 +46,6 @@ const drawImageDataWithTransparency = (
   }
 
   ctx2.putImageData(imageData, 0, 0);
-  replaceColor(ctx2, color);
-
   ctx.drawImage(canvas2, 0, 0);
 };
 
@@ -48,8 +54,9 @@ export const drawMask = async (
   canvas: Canvas,
   color = BLACK
 ) => {
-  const coloredPartImage = await segmentation.mask.toImageData();
-  drawImageDataWithTransparency(canvas, coloredPartImage, color);
+  const imageData = await segmentation.mask.toImageData();
+  replaceColor(imageData, color);
+  drawImageDataWithTransparency(canvas, imageData);
 };
 
 export const drawSkeleton = (pose: Pose, canvas: Canvas, color = BLACK) => {
