4d79830d8a7fbafbb2777b8f86c717918a17906d
Aidan Feldman <aidan.feldman@gmail.com>
2023-01-28 02:52:21 -0500

    fix the color of the mask

--- src/js/utils/segment_helpers.ts
+++ src/js/utils/segment_helpers.ts
@@ -2,13 +2,23 @@ import Skeleton from "../poses/skeleton";
 import { Pose } from "@tensorflow-models/pose-detection";
 import { Segmentation } from "@tensorflow-models/pose-detection/dist/shared/calculators/interfaces/common_interfaces";
 import Canvas from "../display/canvas";
-import { BLACK } from "./colors";
+import { BLACK, toString } from "./colors";
+import { Color } from "@tensorflow-models/pose-detection/dist/shared/calculators/interfaces/common_interfaces";
+
+const replaceColor = (ctx: OffscreenCanvasRenderingContext2D, color: Color) => {
+  const colorStr = toString(color);
+  // https://stackoverflow.com/a/16228281/358804
+  ctx.fillStyle = colorStr;
+  ctx.globalCompositeOperation = "source-in";
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+};
 
 let canvas2: OffscreenCanvas | undefined;
 
 const drawImageDataWithTransparency = (
   canvas: Canvas,
-  imageData: ImageData
+  imageData: ImageData,
+  color = BLACK
 ) => {
   const ctx = canvas.context();
 
@@ -28,6 +38,8 @@ const drawImageDataWithTransparency = (
   }
 
   ctx2.putImageData(imageData, 0, 0);
+  replaceColor(ctx2, color);
+
   ctx.drawImage(canvas2, 0, 0);
 };
 
@@ -37,7 +49,7 @@ export const drawMask = async (
   color = BLACK
 ) => {
   const coloredPartImage = await segmentation.mask.toImageData();
-  drawImageDataWithTransparency(canvas, coloredPartImage);
+  drawImageDataWithTransparency(canvas, coloredPartImage, color);
 };
 
 export const drawSkeleton = (pose: Pose, canvas: Canvas, color = BLACK) => {
