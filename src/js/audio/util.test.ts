56edf43716cc73adc9113b9055e3a91b357a7f0e
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-02 15:01:40 -0500

    use mock classes for Web Speech API objects

--- src/js/audio/util.test.ts
+++ src/js/audio/util.test.ts
@@ -1,23 +1,47 @@
 import { describe, test, expect } from "@jest/globals";
 import { getCommand } from "./util";
 
+// Web Speech API classes aren't available in Node, so create mocks
+
+class SpeechList<Type> {
+  [index: number]: Type;
+  length: number;
+
+  constructor(items: Type[]) {
+    this.length = items.length;
+    for (const [index, item] of items.entries()) {
+      this[index] = item;
+    }
+  }
+
+  item(index: number) {
+    return this[index];
+  }
+}
+
+class MockSpeechRecognitionResult extends SpeechList<SpeechRecognitionAlternative> {
+  isFinal: boolean;
+
+  constructor(items: SpeechRecognitionAlternative[], isFinal: boolean) {
+    super(items);
+    this.isFinal = isFinal;
+  }
+}
+
+class MockSpeechRecognitionResultList extends SpeechList<SpeechRecognitionResult> {}
+
 describe("getting command", () => {
   test("single word", () => {
-    const alt: SpeechRecognitionAlternative = {
-      confidence: 1,
-      transcript: "hello",
-    };
-    const result: SpeechRecognitionResult = {
-      isFinal: true,
-      0: alt,
-      length: 1,
-      item: (_index: number) => alt,
-    };
-    const results: SpeechRecognitionResultList = {
-      0: result,
-      length: 1,
-      item: (_index: number) => result,
-    };
+    const result = new MockSpeechRecognitionResult(
+      [
+        {
+          confidence: 1,
+          transcript: "hello",
+        },
+      ],
+      true
+    );
+    const results = new MockSpeechRecognitionResultList([result]);
 
     const command = getCommand(results);
     expect(command).toEqual("hello");
