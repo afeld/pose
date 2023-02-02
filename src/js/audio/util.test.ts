94a68ff6f2b18e57b0e991e86739bf84fb392996
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-02 15:24:34 -0500

    test to ensure it's not using past speech results

--- src/js/audio/util.test.ts
+++ src/js/audio/util.test.ts
@@ -37,4 +37,29 @@ describe("getCommands()", () => {
     const commands = getCommands(results);
     expect(commands).toEqual(["hello", "world"]);
   });
+
+  test("multiple results should just give the last", () => {
+    const result1 = new MockSpeechRecognitionResult(
+      [
+        {
+          confidence: 1,
+          transcript: "   hello world ",
+        },
+      ],
+      true
+    );
+    const result2 = new MockSpeechRecognitionResult(
+      [
+        {
+          confidence: 1,
+          transcript: "see ya ",
+        },
+      ],
+      true
+    );
+    const results = new MockSpeechRecognitionResultList([result1, result2]);
+
+    const commands = getCommands(results);
+    expect(commands).toEqual(["see", "ya"]);
+  });
 });
--- src/js/audio/util.ts
+++ src/js/audio/util.ts
@@ -1,9 +1,7 @@
-export const getCommands = (
-  recognitionResults: SpeechRecognitionResultList
-) => {
-  // since `continuous = true`, there may be multiple
+export const getCommands = (result: SpeechRecognitionResultList) => {
+  // Since `continuous = true`, past results will be included. Just use the most recent one.
   // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognitionResultList
-  const lastResult = recognitionResults[recognitionResults.length - 1];
+  const lastResult = result[result.length - 1];
   // since `maxAlternatives = 1`, we only need the first result
   const lastCommand = lastResult[0].transcript;
   return lastCommand.toLowerCase().trim().split(/\s+/);
