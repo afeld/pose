5cc4a511335e609b1c99e5840edf56cfb9b5f1bb
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-03 00:04:18 -0500

    only do color conversion once

--- src/js/audio/bluetooth.ts
+++ src/js/audio/bluetooth.ts
@@ -36,18 +36,17 @@ export const updateBluetoothStatus = async () => {
   showBluetoothWarning = !isBluetooth;
 };
 
+const baseColor = colors.RED;
+const redMax = colors.toRGBA(Object.assign({}, baseColor, { a: 0.5 }));
+const redMin = colors.toRGBA(Object.assign({}, baseColor, { a: 0 }));
+const pctOfHeight = 0.08;
+
 /**
  * if a warning should be shown, draws an indicator on the screen
  */
 export const drawNoBluetoothWarning = (canvas: Canvas) => {
   if (showBluetoothWarning) {
     const ctx = canvas.context;
-
-    const baseColor = colors.RED;
-    const redMax = Object.assign({}, baseColor, { a: 0.5 });
-    const redMin = Object.assign({}, baseColor, { a: 0 });
-
-    const pctOfHeight = 0.08;
 
     // bottom
 
@@ -59,8 +58,8 @@ export const drawNoBluetoothWarning = (canvas: Canvas) => {
       0,
       rect1YStart
     );
-    gradient1.addColorStop(0, colors.toRGBA(redMax));
-    gradient1.addColorStop(1, colors.toRGBA(redMin));
+    gradient1.addColorStop(0, redMax);
+    gradient1.addColorStop(1, redMin);
     ctx.fillStyle = gradient1;
 
     ctx.fillRect(0, rect1YStart, canvas.width(), canvas.height());
@@ -70,8 +69,8 @@ export const drawNoBluetoothWarning = (canvas: Canvas) => {
     const rect2YStop = canvas.height() * pctOfHeight;
 
     const gradient2 = ctx.createLinearGradient(0, 0, 0, rect2YStop);
-    gradient2.addColorStop(0, colors.toRGBA(redMax));
-    gradient2.addColorStop(1, colors.toRGBA(redMin));
+    gradient2.addColorStop(0, redMax);
+    gradient2.addColorStop(1, redMin);
     ctx.fillStyle = gradient2;
 
     ctx.fillRect(0, 0, canvas.width(), rect2YStop);
