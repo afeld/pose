f2739659219da9aabe4b20f87a1e6ece0f7e9ab7
Aidan Feldman <aidan.feldman@gmail.com>
2023-02-03 01:48:05 -0500

    display the volume on a Stats panel

--- src/js/sandbox.ts
+++ src/js/sandbox.ts
@@ -1,9 +1,16 @@
+import monitor from "./display/monitor";
+import Stats from "stats.js";
 // https://parceljs.org/languages/javascript/#worklets
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import workletUrl from "worklet:./audio/volume.ts";
 
 const run = async () => {
+  const stats = monitor();
+  // https://github.com/mrdoob/stats.js/blob/master/examples/custom.html
+  const volumePanel = stats.addPanel(new Stats.Panel("volume", "#f8f", "#212"));
+  stats.showPanel(3);
+
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: false,
@@ -16,9 +23,11 @@ const run = async () => {
 
   const node = new AudioWorkletNode(audioContext, "volume-meter");
   node.port.onmessage = (event) => {
-    const volume = event.data;
     // https://github.com/GoogleChromeLabs/web-audio-samples/blob/eed2a8613af551f2b1d166a01c834e8431fdf3c6/src/audio-worklet/basic/volume-meter/main.js#L13
-    console.log(volume * 500);
+    const volume = event.data * 500;
+    console.log(volume);
+
+    volumePanel.update(volume, 100);
   };
 
   const microphone = audioContext.createMediaStreamSource(stream);
