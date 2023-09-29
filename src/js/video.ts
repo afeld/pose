76740fcd5d279feb94fd0479eb2e1a38362c9f13
Aidan Feldman <aidan.feldman@gmail.com>
2023-09-29 14:32:02 -0400

    pause video when tab not visible

--- src/js/video.ts
+++ src/js/video.ts
@@ -1,8 +1,10 @@
 // restart video when tab becomes visible
 const video = document.getElementsByTagName("video")[0] as HTMLVideoElement;
 document.addEventListener("visibilitychange", () => {
-  if (!document.hidden) {
+  if (document.hidden) {
+    video.pause();
     video.currentTime = 0;
+  } else {
     video.play();
   }
 });
