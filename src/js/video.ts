129d6b99543adea6afe49ee2d5ef6873d93fbef7
Aidan Feldman <aidan.feldman@gmail.com>
2023-09-30 14:43:19 -0400

    switch "what kills the stars" animation to an image

--- src/js/video.ts
+++ src/js/video.ts
@@ -1,10 +1,30 @@
 // restart video when tab becomes visible
-const video = document.getElementsByTagName("video")[0] as HTMLVideoElement;
+const video = document.getElementsByTagName("video")[0] as
+  | HTMLVideoElement
+  | undefined;
+const img = document.getElementsByTagName("img")[0] as
+  | HTMLImageElement
+  | undefined;
+
 document.addEventListener("visibilitychange", () => {
   if (document.hidden) {
-    video.pause();
-    video.currentTime = 0;
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
+    }
+    if (img) {
+      img.classList.remove("visible");
+    }
   } else {
-    video.play();
+    if (video) {
+      video.play();
+    }
+    if (img) {
+      img.classList.add("visible");
+    }
   }
 });
+
+if (img) {
+  img.classList.add("visible");
+}
